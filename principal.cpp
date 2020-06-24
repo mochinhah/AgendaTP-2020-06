dc5e8789c7a9fa089c818a4f0e266f66173703fd
Morgan <mochinhah@gmail.com>
2020-06-23 23:07:09 -0300

    Melhorei os comentários
    Adicionei comentários ao loop que repete a mensagem de boas vindas

---
+++ principal.cpp
@@ -0,0 +1,9 @@
+
+#include <iosdtream>
+// Início do programa
+int main(int argc, char** argv){
+	// Repete a mensagem de boas vindas
+	// for (int i=0; i<100; i++){
+	std::cout << "Programa iniciado" << std::endl;
+        }
+}
